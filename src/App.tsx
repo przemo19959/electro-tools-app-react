fd62ae464e5164c9534c592d58694b8c0320f25b
pdabrowski <przemyslaw.dabrowski95@gmail.com>
2026-02-21 20:55:18 +0100

    initial commit, MUI added, basic view layout started

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import './App.css'
+import {AppBar, Button, IconButton, Toolbar} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import {Home} from "./views/home/home";
+import styled from "@emotion/styled";
+import {Sidenav} from "./components/sidenav/Sidenav";
+import {useState} from "react";
+
+function App() {
+  const [sidenavOpen, setSidenavOpen] = useState(false);
+
+  return (
+    <div>
+      <StyledAppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{mr: 2}}
+            onClick={() => setSidenavOpen(!sidenavOpen)}
+          >
+            <MenuIcon/>
+          </IconButton>
+          <div>News</div>
+          <div style={{flex: 1}}/>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </StyledAppBar>
+      <StyledContainer>
+        <Sidenav open={sidenavOpen}/>
+        <Home/>
+      </StyledContainer>
+    </div>
+  )
+}
+
+export default App
+
+const StyledAppBar = styled(AppBar)`
+  height: 48px !important;
+  
+  .MuiToolbar-root {
+    min-height: 48px !important;
+  }
+`;
+
+const StyledContainer = styled.div`
+  height: calc(100vh - 48px);
+  overflow: auto;
+  display: flex;
+`;
---
+++ src/components/sidenav/Sidenav.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+type SidenavProps = {
+  open: boolean;
+}
+
+export const Sidenav = ({
+                          open,
+                        }: SidenavProps) => {
+  if(!open) return null;
+
+  return (
+    <StyledContainer>
+      <div style={{marginBottom: 16}}>Home</div>
+    </StyledContainer>
+  );
+};
+
+const StyledContainer = styled.div`
+  width: 200px;
+  background-color: #f0f0f0;
+`;
